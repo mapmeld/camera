5a3863322116560605762b0e124e613d469eb5d0
Nick Doiron <ndoiron@mapmeld.com>
2017-10-31 16:31:35 -0400

    css and https fixes

--- static/scripts/force-https.js
+++ static/scripts/force-https.js
@@ -1,5 +1,5 @@
 // require localhost or HTTPS origin for this page
 // required for Chrome browser to use getUserMedia()
-if (window.location.host !== 'localhost' && window.location.protocol !== 'https:') {
+if (window.location.host.split(':')[0] !== 'localhost' && window.location.protocol !== 'https:') {
    window.location.href = window.location.href.replace(window.location.protocol, 'https:');
 }
