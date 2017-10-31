08dbcbf12c6584b801d9140b1f4e4b1b2d499d0a
Nick Doiron <ndoiron@mapmeld.com>
2017-10-31 10:33:52 -0400

    prepare list of cameras

--- scripts/imagecapture.js
+++ scripts/imagecapture.js
@@ -17,7 +17,7 @@
  * limitations under the License.
  */
 
-export let ImageCapture = window.ImageCapture;
+//export let ImageCapture = window.ImageCapture;
 
 if (typeof ImageCapture === 'undefined') {
   ImageCapture = class {
